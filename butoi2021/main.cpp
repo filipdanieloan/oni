9013f66d6a283c2699205681988f4e92d38ddd66
filipdanieloan <filip.oancea@gmail.com>
2025-03-25 13:49:15 +0200

    butoi2021, niciun cod

---
+++ butoi2021/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ifstream fin("butoi.in");
+ofstream fout("butoi.out");
+
+int c, v, n, k, p, a[100001];
+
+int main(){
+    fin >> c >> v >> n >> k >> p;
+    for(int i = 1; i <= n; ++i){
+        fin >> a[i];
+    }
+    if(c == 1){
+
+    }
+}
