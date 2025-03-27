95d75c7abbd20853e1164bb309eb00a193164a47
filipdanieloan <filip.oancea@gmail.com>
2025-03-27 20:35:07 +0200

    Mai multe adaugari

--- veciniPari.pbinfo/main.cpp
+++ veciniPari.pbinfo/main.cpp
@@ -1,9 +1,10 @@
+//60p
 #include <bits/stdc++.h>
 using namespace std;
 
 int n,m,matrice[101][101],c,px,py;
-int dx[] = {0, 1, 0, -1};
-int dy[] = {1, 0, -1, 0};
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
 
 int main(){
     cin >> n >> m;
@@ -14,10 +15,16 @@ int main(){
     }
     for(int i = 1; i <= n; ++i){
         for(int j = 1; j <= m; ++j){
+            bool ok = true;
             for(int k = 0; k < 4; ++k){
                 px = j + dx[k];
                 py = i + dy[k];
+                if(matrice[py][px] % 2 == 1)
+                    ok = false;
             }
+            if(ok == true)
+                ++c;
         }
     }
+    cout << c;
 }
