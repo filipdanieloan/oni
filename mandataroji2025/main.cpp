bef9a6e00451c5837b6b0a27b5edf23e03497fd2
filipdanieloan <filip.oancea@gmail.com>
2025-03-29 10:12:55 +0200

    Mandatar 80p

--- mandataroji2025/main.cpp
+++ mandataroji2025/main.cpp
@@ -4,12 +4,38 @@ using namespace std;
 ifstream fin("mandatar.in");
 ofstream fout("mandatar.out");
 
-int c, a[100001], b[100001], n, d, cnt1, p, cop;
+int c, a[100001], b[100001], n, d, cnt1, p, cop, nrmax2 = INT_MIN;
+int maxfp = INT_MIN, fp;
 
 int main(){
     fin >> c >> n;
+    fill(b+1, b+n+1, 1);
     for(int i = 1; i <= n; ++i){
         fin >> a[i];
+        if(c >= 2){
+            fp = 0;
+            cnt1 = 1;
+            d = 2;
+            cop = a[i];
+            while(cop > 1){
+                if(cop % d == 0){
+                    b[i] *= d;
+                    ++fp;
+                }
+                while(cop % d == 0)
+                    cop /= d;
+                ++d;
+                if(d * d > cop)
+                    d = cop;
+            }
+            if(fp > maxfp){
+                maxfp = fp;
+                nrmax2 = b[i];
+            }
+            else if(fp == maxfp){
+                nrmax2 = max(nrmax2, b[i]);
+            }
+        }
     }
     sort(a+1, a+n+1);
     if(c == 1){
@@ -34,5 +60,9 @@ int main(){
                 return 0;
             }
         }
+    }
+    else if(c == 2){
+        fout << nrmax2;
+        return 0;
     }
 }
